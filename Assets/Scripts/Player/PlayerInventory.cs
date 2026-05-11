30ce18c540cdfc2f554278a9b087b7e3c1e2bf43
TommyyPasto <tommasopastorelli39@gmail.com>
2026-05-11 19:49:46 +0200

    [FIX] rollback done on PlayerInventory.cs

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -87,11 +87,6 @@ public class PlayerInventory : MonoBehaviour
         if (slotIndex < 0 || slotIndex >= slots.Length)
             return;
 
-
-        //AGGIUNTO PER TESTARE SCORRIMENTO e SELEZIONE CORRETTA SLOT
-        SelectedSlot = slotIndex;
-        Debug.Log("Slot selected: " + SelectedSlot); 
-
         DeactivateAllSlots();
 
         SelectedSlot = slotIndex;
