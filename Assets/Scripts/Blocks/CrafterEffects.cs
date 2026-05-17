d2d7d1dde68747edfa51719fb63fcacb7394e96b
Riccardo <riccardoplay2003@gmail.com>
2026-05-17 04:31:17 +0200

    [ADD] Saw Particle Effects
    Added particles when saw is in processing state.

---
+++ Assets/Scripts/Blocks/CrafterEffects.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class CrafterEffects : MonoBehaviour
+{
+    [SerializeField]
+    private CrafterBlock crafter;
+
+    [SerializeField]
+    private ParticleSystem processingParticles;
+
+    [SerializeField]
+    private float particleInterval = 0.2f;
+
+    private float particleTimer;
+
+    private void Update()
+    {
+        bool isProcessing =
+            crafter.state ==
+            CrafterState.Processing;
+
+        if (isProcessing)
+        {
+            particleTimer -= Time.deltaTime;
+
+            if (particleTimer <= 0f)
+            {
+                processingParticles.Play();
+
+                particleTimer =
+                    particleInterval;
+            }
+        }
+    }
+}
