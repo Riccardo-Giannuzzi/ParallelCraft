ecbb19b3eed3b5f201dee5def492d37c28ac73ed
Timour-Ilyas <timour.computer@gmail.com>
2026-05-19 20:32:13 +0200

    Functional menu

---
+++ Assets/Scripts/menu/MenuManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
